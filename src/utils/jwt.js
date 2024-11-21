cc1fdeff05b97f844bf7bc85ae36f0f392ddb1ba
ashinjoy <ashinjoy22@gmail.com>
2024-11-21 05:52:37 +0530

    token

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -4,7 +4,7 @@ const {JWT_SECRET} = secret
 
 export const createAccessToken = async(payload)=>{
     try {
-       return await jwt.sign(payload,JWT_SECRET)
+       return await jwt.sign(payload,"893893893IUEIUEWIUEIU2932932932SDJSDKJSKJ")
     } catch (error) {
         console.error(error);
         throw error
@@ -14,7 +14,7 @@ export const createAccessToken = async(payload)=>{
 
 export const decodeRefreshToken = async(token)=>{
     try {
-        return await jwt.verify(token,JWT_SECRET)
+        return await jwt.verify(token,"893893893IUEIUEWIUEIU2932932932SDJSDKJSKJ")
     } catch (error) {
         console.error(error);
         throw error
